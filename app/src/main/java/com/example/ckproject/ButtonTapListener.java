678a92f34f7eb83270f75dc683c1cb21be0335df
MrCrashLab <m.dudarev2002@mail.ru>
2023-05-07 19:09:29 +0300

    Add floor button

---
+++ app/src/main/java/com/example/ckproject/ButtonTapListener.java
@@ -0,0 +1,48 @@
+package com.example.ckproject;
+
+import static com.gun0912.tedpermission.provider.TedPermissionProvider.context;
+
+import android.view.View;
+import android.widget.Button;
+
+import com.example.ckproject.model.Map;
+import com.github.chrisbanes.photoview.PhotoView;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class ButtonTapListener implements View.OnClickListener {
+
+    private int floor;
+    private List<Map> maps;
+    private PhotoView photoView;
+    private Button btn1, btn2, btn3;
+
+    public ButtonTapListener(int floor, Button btn1, Button btn2, Button btn3, List<Map> maps, PhotoView photoView) {
+        this.floor = floor;
+        this.btn1 = btn1;
+        this.btn2 = btn2;
+        this.btn3 = btn3;
+        this.maps = maps;
+        this.photoView = photoView;
+    }
+
+    @Override
+    public void onClick(View view) {
+        view.setBackgroundResource(R.drawable.rounded_floor_btn_press);
+        btn1.setBackgroundResource(R.drawable.rounded_floor_btn);
+        btn2.setBackgroundResource(R.drawable.rounded_floor_btn);
+        btn3.setBackgroundResource(R.drawable.rounded_floor_btn);
+        for(Map m: maps){
+            if(m.getFloor() == floor){
+                Picasso
+                        .with(context)
+                        .load(m.getSrc())
+                        .resize(2048, 2048)
+                        .onlyScaleDown()
+                        .into(photoView);
+                break;
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/ckproject/ParkingMapActivity.java
+++ app/src/main/java/com/example/ckproject/ParkingMapActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
@@ -29,8 +30,10 @@ import retrofit2.http.Path;
 public class ParkingMapActivity extends AppCompatActivity {
 
     private PhotoView photoView;
+    private Button btn1, btn2, btn3, btn4;
     private int id;
     private int floor = 1;
+    private int floor_count = 0;
     private final String BASE_URL = "http://192.168.0.106:8000";
     private List<Map> maps;
     private LinearLayout btnGroup;
@@ -52,6 +55,10 @@ public class ParkingMapActivity extends AppCompatActivity {
         photoView.setZoomable(false);
         photoView.setMinimumScale(0.5F);
         photoView.setScale(0.5F);
+        btn1 = findViewById(R.id.btn_floor1);
+        btn2 = findViewById(R.id.btn_floor2);
+        btn3 = findViewById(R.id.btn_floor3);
+        btn4 = findViewById(R.id.btn_floor4);
         getImageMap();
     }
 
@@ -66,15 +73,14 @@ public class ParkingMapActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<List<Map>> call, Response<List<Map>> response) {
                 maps = response.body();
+                floor_count = 0;
                 String imageUri = new String("");
                 for (Map m : maps){
                     if(m.getFloor() == floor){
                         imageUri = m.getSrc();
-                        Log.println(Log.ASSERT,"WTF", String.valueOf(m.getFloor()));
-                        break;
                     }
+                    floor_count++;
                 }
-                Log.println(Log.ASSERT,"WTF",  imageUri);
                 Picasso
                     .with(context)
                     .load(imageUri)
@@ -83,6 +89,11 @@ public class ParkingMapActivity extends AppCompatActivity {
                     .into(photoView);
                 photoView.setZoomable(true);
                 btnGroup.setVisibility(View.VISIBLE);
+                disableBtn();
+                btn1.setOnClickListener(new ButtonTapListener(1, btn2, btn3, btn4, maps, photoView));
+                btn2.setOnClickListener(new ButtonTapListener(2, btn1, btn3, btn4, maps, photoView));
+                btn3.setOnClickListener(new ButtonTapListener(3, btn1, btn2, btn4, maps, photoView));
+                btn4.setOnClickListener(new ButtonTapListener(4, btn1, btn2, btn3, maps, photoView));
             }
 
             @Override
@@ -90,5 +101,18 @@ public class ParkingMapActivity extends AppCompatActivity {
                 Toast.makeText(ParkingMapActivity.this, "You have internet problem!", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+    private void disableBtn(){
+        switch (floor_count){
+            case 4:
+                break;
+            case 1:
+                btn2.setVisibility(View.GONE);
+            case 2:
+                btn3.setVisibility(View.GONE);
+            case 3:
+                btn4.setVisibility(View.GONE);
+                break;
+        }
     }
 }
